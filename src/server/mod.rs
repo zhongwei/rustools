6ffcf5c898367c0c78488f95aaa4ebb2e5030599
Zhong Wei <zhongwei99@163.com>
2017-11-23 21:59:05 +0800

    add web static assets

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,11 +1,21 @@
+use std::io;
+use std::path::{Path, PathBuf};
+
 use rocket;
+use rocket::response::NamedFile;
 
 pub fn start() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, files]).launch();
 }
 
 #[get("/")]
-pub fn index() -> &'static str {
-    "Hello, 钟逸鸣!"
+pub fn index() -> io::Result<NamedFile> {
+    NamedFile::open("static/index.html")
 }
+
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
 
